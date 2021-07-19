557a78ac8f1b1a55f4d09a8129b0ed7ee4dee840
MONSTER DIVE WEBプロダクション事業部 <md-web@monster-dive.com>
2021-07-19 21:57:22 +0900

    サポート情報やサンプルコードを公開（CHAPTER 04-022）

---
+++ sample-app/src/components/pages/PageCarouselItem.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageCarouselItem"
+});
+</script>
+
+<template>
+  <li class="Carousel__item">
+    <slot />
+  </li>
+</template>
+
+<style lang="scss" scoped></style>
