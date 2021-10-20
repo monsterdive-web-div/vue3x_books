9a8ac65c743881d781bbac8bdeb15cebfa26c063
MONSTER DIVE WEBプロダクション事業部 <md-web@monster-dive.com>
2021-10-20 21:47:10 +0900

    CHAPTER 02のサンプルコードをセクションごとに分割（Chapter2-008）

---
+++ sample-app/src/views/Top.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Top"
+});
+</script>
+
+<template>
+  <div class="top">
+    <h1>This is an top page</h1>
+  </div>
+</template>
